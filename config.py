e0bccaa282e786ad77277c18439e5090a60c2f2e
kurdstan11 <106327964+kurdstan11@users.noreply.github.com>
2023-02-16 02:24:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,27 +5,27 @@ admins = {}
 load_dotenv()
 
 # client vars
-API_ID = int(getenv("API_ID", "7452578")) #لا تغير هاذة القيمة
-API_HASH = getenv("API_HASH", "061d67ee8eed9368c5cadabb4aa21efc")#لا تغير هاذة القيمة
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-SESSION_NAME = getenv("SESSION_NAME", "")
+API_ID = int(getenv("API_ID", "13980703")) #لا تغير هاذة القيمة
+API_HASH = getenv("API_HASH", "8739dceb2d5ee894b67b106efc50d52c")#لا تغير هاذة القيمة
+BOT_TOKEN = getenv("BOT_TOKEN", "5861877575:AAFC0i9Tgwcuqadnbq3fTE0EwoXYATSbvFU")
+SESSION_NAME = getenv("SESSION_NAME", "1ApWapzMBu24oS_bBbNqASrCOCXvF572VjiLr0Pfuej2T-uTRMb9usoTafL4xO25RET_x4iWqY4KwJ51leHECMrerKUYxSekPn4YdPcXd7A2rlSvCIbPlmgofw9PXf-rrrToaC0MSxEFX0wBD6l4X0uv5inyFjdX93CzJzkSVfa1fIMm36VUNUJuSlI_sO0O0Zn8Qxqbjr-j_uiCKRJkKjhAfmjrSgjmOULCWG2ICXMA5fCSNCMgTE3TUSqdVn-OJGefN3ipDZkaalgu0NQ7TaEyXGyCyOA89iprTOMifhu4aYwXHJT0bqMqHgdNucZog0kTebhdP6XRR_qwyGch0MeBS8ogNAMo=")
 
 # mandatory vars
-OWNER_USERNAME = getenv("OWNER_USERNAME", "rr8r9") # @ هنا ضع يوزر حسابك بدون 
-ALIVE_NAME = getenv("ALIVE_NAME", "sonng") # هنا ضع اسم حسابك
-BOT_USERNAME = getenv("BOT_USERNAME", "") # @ هنا ضع يوزر البوت بدون 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/X02lx/RrRRR") 
+OWNER_USERNAME = getenv("OWNER_USERNAME", "kurdi_sport") # @ هنا ضع يوزر حسابك بدون 
+ALIVE_NAME = getenv("ALIVE_NAME", "BEZARM") # هنا ضع اسم حسابك
+BOT_USERNAME = getenv("BOT_USERNAME", "kurdi_sport_BOT") # @ هنا ضع يوزر البوت بدون 
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/kurdstan11/TTTTRrrr85") 
 UPSTREAM_BRANCH = getenv("UPSTREM_BRANCH", "main") #لا تغير هاذة القيمة
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60")) #لا تغير هاذة القيمة
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "rr8r9") # @ هنا ضغ يوزر كروبك بدون 
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "xl444") # @ هنا ضغ يوزر قناتك بدون
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "Reklam_jat") # @ هنا ضغ يوزر كروبك بدون 
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "Hzrvany_savy") # @ هنا ضغ يوزر قناتك بدون
 
 # database, decorators, handlers mandatory vars
 MONGODB_URL = getenv("MONGODB_URL", "mongodb+srv://veez:mega@cluster0.heqnd.mongodb.net/veez?retryWrites=true&w=majority")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! . $").split())
-OWNER_ID = list(map(int, getenv("OWNER_ID", "1854384004").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "5799939262").split()))
                                              #هنا ضع ايدي المطور فوق و الاعلئ
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1854384004").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5799939262").split()))
 
 # image resources vars
 IMG_1 = getenv("IMG_1", "https://te.legra.ph/file/2a726c634dbc3b9e8f451.png")
